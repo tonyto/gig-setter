70ace38e6406c248a677387575150b5b3616eab9
tonyto <tonyto85@gmail.com>
2011-10-22 13:00:47 +0100

    changed env port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('/', function(req, res){
     title: 'Express'
   });
 });
-
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
