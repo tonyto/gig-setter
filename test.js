3a6ce689cb4ea9e2cb9966be8f9226b5f8e21e75
Greg Sochanik <greg@sochanik.com>
2011-10-23 09:26:28 +0000

    I dont know what im doing :-P

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 var sys = require('sys');
+var eyes = require('eyes');
 var storer = require('./lib/conversation-storer.js');
 
 var conversationKey= 'tony_matt_22102011141414';
 var conversationKey2= 'greg_tonyd_22102011141414';
+var conversationKey3= 'greg_tonyd_22102011141414987687';
 var conversation = ['sausage','beans','egg','farts', 'tarts'];
 var conversation2 = ['kylie','jason','json','ballache'];
+var conversation3 = ['winter', 'spring', 'summer','donner','kebab','greasy'];
 
-storer.store(conversationKey, conversation);
-storer.store(conversationKey2, conversation2);
-//storer.retrieve(conversationKey);
+eyes.inspect(storer.conversationStore);
 
+storer.conversationStore.store(conversationKey, conversation);
+storer.conversationStore.store(conversationKey3, conversation2);
+storer.conversationStore.store(conversationKey2, conversation3);
