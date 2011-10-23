257f2b6438f217316076892beae987447d842d6d
Antony Denyer <antonydenyer@hotmail.com>
2011-10-23 12:14:21 +0100

    addword

--- test/test-conversation-storer.js
+++ test/test-conversation-storer.js
@@ -2,7 +2,11 @@ var conversationStore = require("../lib/conversation-storer").conversationStore;
 
 exports["should return false when there are no duplicates "] = function (test) {
     var key = 'Greg_Tony',
-        player = 'Tony',
+        player = 'Tony';
+        conversationStore.addWord(key,player,'hello');
+        conversationStore.addWord(key,player,'is');
+    
+
         result = conversationStore.checkForDuplicates(key,player);
 
         test.equal(result,false);
@@ -11,7 +15,11 @@ exports["should return false when there are no duplicates "] = function (test) {
 
 exports["should return true when there are duplicates "] = function (test) {
     var key = 'Greg_Tony',
-        player = 'Greg',
+        player = 'Greg';
+
+        conversationStore.addWord(key,player,'hello');
+        conversationStore.addWord(key,player,'hello');
+
         result = conversationStore.checkForDuplicates(key,player);
             test.equal(result, true);
             test.done();
